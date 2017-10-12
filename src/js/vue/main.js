73de2191aca4e9ca6ad519f9fd18f64e63e995d8
AbraXabra <htmlmanpro@gmail.com>
2017-10-12 18:16:26 +0300

    + element-ui

--- src/js/vue/main.js
+++ src/js/vue/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import router from '../vue/router';
-
+import ElementUI from 'element-ui';
 import Content from '../vue/components/Content.vue'
+
+Vue.use(ElementUI);
 
 
 new Vue({
